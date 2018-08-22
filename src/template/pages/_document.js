a7b62e7537367c3f252c53286cb383997515ba20
Kyle Kelley <rgbkrk@gmail.com>
2018-08-22 01:13:31 -0400

    bring in some css and typography for default style

---
+++ src/template/pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/awsm.css/dist/awsm.min.css"
+          />
+          <style>{`body { margin: 0 } /* custom! */`}</style>
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
